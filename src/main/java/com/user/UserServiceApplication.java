3cb4a35e49175ac7e17c2882c8d7d0e792059168
UtsavDabhi16 <utsav.inexture@gmail.com>
2022-06-10 18:42:10 +0530

    10/06/2022

--- src/main/java/com/user/UserServiceApplication.java
+++ src/main/java/com/user/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package com.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -17,6 +20,13 @@ public class UserServiceApplication {
 	@LoadBalanced
 	RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+
+	@Autowired
+	public void getEnv(Environment e)
+	{
+		System.out.println("utsav: "+e.getProperty("user.msg"));
 	}
 
 }
--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.user.controller;
 
 import com.user.Service.UserService;
-import com.user.entity.Contact;
 import com.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +17,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+
+    @Value("${user.msg}")
+    private String message;
 
     @GetMapping("/{userId}")
     public User getUser(@PathVariable("userId") Long userId){
@@ -34,10 +41,11 @@ public class UserController {
     }
 
 
-    @GetMapping("/demo")
-    public String display(){
-        return "Hello World";
-    }
 
+    @GetMapping("/message")
+    public String getMessage(){
+        System.out.println("Inside get message method !!!!");
+        return message;
+    }
 
 }
